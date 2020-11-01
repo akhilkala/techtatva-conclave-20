4d76011eb2c8bf8887c9fa065c9893cfb1f14d2c
Akhil <kalaakki@gmail.com>
2020-11-01 11:26:44 +0530

    removing react

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,111 +1,77 @@
 import React, { useState, useRef } from "react";
 import "./Register.scss";
+import { Formik } from "formik";
 
 export default function Register() {
-  const initialState = {
-    name: "",
-    email: "",
-    contact: "",
-    collage: "",
-    registeration: "",
-  };
-
-  const [form, setForm] = useState(initialState);
-
-  const [placeholders, setPlaceholders] = useState(initialState);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    console.log(form);
-
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const phoneRegex = /^\d{10}$/;
-
-    if (!emailRegex.test(form.email.toLowerCase())) {
-      setForm({ ...form, email: "" });
-      setPlaceholders({ ...placeholders, email: "Invalid Email" });
-    }
-
-    if (!phoneRegex.test(form.contact.toLowerCase())) {
-      setForm({ ...form, contact: "" });
-      setPlaceholders({ ...placeholders, contact: "Invalid Phone Number" });
-    }
-
-    if (form.name === "") {
-      setPlaceholders({ ...placeholders, name: "This Field is required" });
-    }
-
-    if (form.collage === "") {
-      setPlaceholders({ ...placeholders, collage: "This Field is required" });
-    }
-
-    if (form.registeration === "") {
-      setPlaceholders({
-        ...placeholders,
-        registeration: "This Field is required",
-      });
-    }
-  };
-
   return (
     <div className="register">
-      <form className="register__form">
-        <h1 className="register__title">REGISTER</h1>
-        <div className="register__inputs">
-          <Input
-            value={form.name}
-            onChange={(e) => setForm({ ...form, name: e.target.value })}
-            name={"Name"}
-            placeholder={placeholders.name}
-          />
+      <Formik
+        onSubmit={(data) => console.log(data)}
+        initialValue={{
+          name: "",
+          email: "",
+          contact: "",
+          collage: "",
+          registeration: "",
+        }}
+      >
+        {({ handleSubmit, handleChange, handleBlur }) => {
+          return (
+            <form onSubmit={handleSubmit} className="register__form">
+              <h1 className="register__title">REGISTER</h1>
+              <div className="register__inputs">
+                <Input
+                  nameKey={"name"}
+                  handleChange={handleChange}
+                  name={"Name"}
+                  handleBlur={handleBlur}
+                />
 
-          <Input
-            value={form.email}
-            onChange={(e) => setForm({ ...form, email: e.target.value })}
-            name={"Email ID"}
-            placeholder={placeholders.email}
-          />
+                <Input
+                  nameKey={"email"}
+                  handleChange={handleChange}
+                  name={"Email ID"}
+                  handleBlur={handleBlur}
+                />
 
-          <Input
-            value={form.contact}
-            onChange={(e) => setForm({ ...form, contact: e.target.value })}
-            name={"Contact no"}
-            placeholder={placeholders.contact}
-          />
+                <Input
+                  nameKey={"contact"}
+                  handleChange={handleChange}
+                  name={"Contact no"}
+                  handleBlur={handleBlur}
+                />
 
-          <Input
-            value={form.collage}
-            onChange={(e) => setForm({ ...form, collage: e.target.value })}
-            name={"College name"}
-            placeholder={placeholders.collage}
-          />
+                <Input
+                  nameKey={"collage"}
+                  handleChange={handleChange}
+                  name={"College name"}
+                  handleBlur={handleBlur}
+                />
 
-          <Input
-            value={form.registeration}
-            onChange={(e) =>
-              setForm({ ...form, registeration: e.target.value })
-            }
-            name={"Reg no. (For MAHE students only)"}
-            placeholder={placeholders.registeration}
-          />
-        </div>
-        <button onClick={(e) => handleSubmit(e)} className="register__submit">
-          SUBMIT
-        </button>
-      </form>
+                <Input
+                  nameKey={"registeration"}
+                  handleChange={handleChange}
+                  name={"Reg no. (For MAHE students only)"}
+                  handleBlur={handleBlur}
+                />
+              </div>
+              <button className="register__submit">SUBMIT</button>
+            </form>
+          );
+        }}
+      </Formik>
     </div>
   );
 }
 
-function Input({ name, value, onChange, placeholder }) {
+function Input({ name, nameKey, handleChange, handleBlur }) {
   return (
     <div className="register__input">
       <label htmlFor={name}>{name}:</label>
       <input
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
+        onBlur={handleBlur}
+        handleChange={handleChange}
+        name={nameKey}
         id={name}
         type="text"
       />
